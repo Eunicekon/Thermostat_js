a483b65dc07ef254f56fa6b6db1b349e51deb0f3
Eunice Kontor <Eunice.Kontor@ft.com>
2020-11-20 20:47:30 +0000

    dom selection

---
+++ interface.js
@@ -0,0 +1,21 @@
+let tempUp = document.getElementById("temp-up");
+let tempDown = document.getElementById("temp-down");
+let tempReset = document.getElementById("temp-reset");
+let powerSaveOn = document.getElementById("powersaving-on");
+let powerSaveOff = document.getElementById("powersaving-off");
+
+
+let defaultTemp = 20;
+let minTemp = 25;
+let maxTemp = 32;
+let resetTemp = 0;
+let powerOn = " ";
+let powerOff = " ";
+
+
+
+for (let i = defaultTemp; i === tempUp; i++){
+  if(i < 25){
+    console.log("up the limit in here");
+  }
+}
